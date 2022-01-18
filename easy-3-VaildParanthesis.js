879a0dcd43bc6cf249f7ccb24835f065d47ffa08
Ishaan Thakur <ishaant99@gmail.com>
2022-01-17 20:44:13 -0400

    Valid-Parenthesis - Easy

---
+++ easy-3-VaildParanthesis.js
@@ -0,0 +1,29 @@
+const isValid = function (s) {
+  const hashMap = {
+    "}": "{",
+    "]": "[",
+    ")": "(",
+  };
+  const stack = [];
+
+  for (let c of s) {
+    if (hashMap.hasOwnProperty(c)) {
+      if (stack && stack[stack.length - 1] === hashMap[c]) {
+        stack.pop();
+      } else {
+        return false;
+      }
+    } else {
+      stack.push(c);
+    }
+  }
+
+  console.log(stack);
+
+  if (!stack.length) return true;
+  else return false;
+};
+
+export default isValid;
+
+// ([){()}[]
--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import twoSum from "./easy-1-TwoSum.js";
 import isPalindrome from "./easy-2-Palindrome.js";
+import isValid from "./easy-3-VaildParanthesis.js";
 
 let answer;
 
 // answer = twoSum([2, 7, 11, 15], 9);
-answer = isPalindrome(1221);
+// answer = isPalindrome(1221);
+answer = isValid("([){()}[]");
 console.log(answer);
