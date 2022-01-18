946076087b37d6619957cd4c5e7a1b074248ec3a
Ishaan Thakur <ishaant99@gmail.com>
2022-01-18 00:59:50 -0400

    Remove Array Duplicates - Easy

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ import twoSum from "./easy-1-TwoSum.js";
 import isPalindrome from "./easy-2-Palindrome.js";
 import isValid from "./easy-3-VaildParanthesis.js";
 import maxNumberOfBalloons from "./easy-4-MaximumNumberOfBallons.js";
+import removeDuplicates from "./easy-5-RemoveDuplicatesFromSortedArray.js";
 
 let answer;
 
 // answer = twoSum([2, 7, 11, 15], 9);
 // answer = isPalindrome(1221);
 // answer = isValid("([){()}[]");
-answer = maxNumberOfBalloons("balon");
+// answer = maxNumberOfBalloons("balon");
+
+answer = removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]);
 console.log(answer);
